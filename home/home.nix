8c772f7fb4a7c88a2431dbfbdeaf942abf6a8b83
BrockoliniMorgan <brockjamesmorgan@gmail.com>
2026-06-30 01:15:34 +1000

    Cleanup

--- home/home.nix
+++ home/home.nix
@@ -5,9 +5,7 @@
     settings = let 
       lua = lib.generators.mkLuaInline;
     in {
-      mod = {
-        _var = "SUPER";
-      };
+      mod._var = "SUPER";
       monitor = {
         output = "";
         mode = "preferred";
@@ -17,17 +15,15 @@
       config = {
         input = {
           repeat_rate = 40;
-          repeat_delay = 300;
+          repeat_delay = 250;
         };
         ecosystem.no_update_news = true;
         general = {
-          gaps_in = 2;
-          gaps_out = 4;
+          gaps_in = 0;
+          gaps_out = 0;
           border_size = 0;
         };
-        decoration = {
-          rounding = 4;
-        };
+        decoration.rounding = 4;
         animations.enabled = false;
         input.touchpad.natural_scroll = true;
       };
@@ -52,16 +48,14 @@
           (lua "hl.dsp.exec_cmd(\"${lib.getExe pkgs.qutebrowser}\")")
         ];
       }];
-      on = {
-        _args = [
-          "hyprland.start"
-          (lua ''
-           function()
-            hl.exec_cmd("waybar")
-           end
-          '')
-        ];
-      };
+      on._args = [
+        "hyprland.start"
+        (lua ''
+         function()
+          hl.exec_cmd("waybar")
+         end
+        '')
+      ];
     };
     # Just using this for functions - can do it in nix, but 
     # it's cleaner when looking at the actual lua config file
