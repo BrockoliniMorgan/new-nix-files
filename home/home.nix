531fb76371b740cd6b187561194da11e3384f8c6
BrockoliniMorgan <brockjamesmorgan@gmail.com>
2026-07-27 12:19:42 +1000

    Fixed qutebrowser adblocking

--- home/home.nix
+++ home/home.nix
@@ -18,6 +18,7 @@
   # :set -u https://accounts.google.com/* content.headers.user_agent "Mozilla/5.0 ({os_info}; rv:135.0) Gecko/20100101 Firefox/135"
   programs.qutebrowser.perDomainSettings."https://accounts.google.com/*".content.headers.user_agent =
     "Mozilla/5.0 ({os_info}; rv:135.0) Gecko/20100101 Firefox/135";
+  programs.qutebrowser.settings.content.blocking.method = "adblock";
   programs.git.settings = {
     user = {
       name = "BrockoliniMorgan";
